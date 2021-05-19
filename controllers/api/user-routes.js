894af0575507fbdc43dd2510c915aeea3d88cbaf
Nancy <nancyrosas0@gmail.com>
2021-05-19 16:33:45 -0500

    added remainder of user route CRUD

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User }  = require('../../models');
+const { User, Post }  = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -34,13 +34,64 @@ router.get('/:id', (req, res) => {
           attributes: ['title']
       }
         },
-        {
-          model: Post,
-          attributes: ['title'],
-          through: Vote,
-          as: 'voted_posts'
-        }
       ]
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'Your email address is not registered.' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password. Please try again.' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in.' });
+    });
+  });
+
+  router.put('/:id', (req, res) => {
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
     })
       .then(dbUserData => {
         if (!dbUserData) {
