e655066eb4b43661aa669d990b6801e71dbdba59
Nancy <nancyrosas0@gmail.com>
2021-05-18 21:25:04 -0500

    added delete route

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize('rate_your_bootcamp', 'root', 'password', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
+
 
 module.exports = sequelize;
