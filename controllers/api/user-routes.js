595e2f1b0999598427521c9d966fec3495247a28
Nancy <nancyrosas0@gmail.com>
2021-05-19 18:27:49 -0500

    troubleshoot put route for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -20,21 +20,7 @@ router.get('/:id', (req, res) => {
       attributes: { excludes: ['password']},
       where: {
         id: req.params.id
-      },
-      include: [
-        {
-          model: Post,
-          attributes: ['id', 'title', 'post_url', 'created_at']
-        },
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'created_at'],
-          include: {
-          model: Post,
-          attributes: ['title']
       }
-        },
-      ]
     })
       .then(dbUserData => {
         if (!dbUserData) {
