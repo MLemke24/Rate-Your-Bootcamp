ab8cb07d850d8bb3721cf5df6df8b06bdf9d02c8
Dakota Nelson <kotalilyy@gmail.com>
2021-05-20 11:28:43 -0500

    begin template

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
+const homeRoutes = require('./home-routes.js');
+
 router.use('/users', userRoutes);
+
+router.use('/', homeRoutes);
 
 module.exports = router;
