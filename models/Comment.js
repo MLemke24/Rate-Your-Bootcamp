410991404f89e1835248b49455e91d49923b2d02
Dakota Nelson <kotalilyy@gmail.com>
2021-05-21 12:42:19 -0500

    general idea/ layout .. continue building

--- models/Comment.js
+++ models/Comment.js
@@ -1,46 +1,46 @@
-const { Model, DataTypes } = require('sequelize');
-const { validate } = require('../config/connection');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const { validate } = require("../config/connection");
+const sequelize = require("../config/connection");
 
 class Comment extends Model {}
 
 Comment.init(
-    {
-        // THESE ARE THE COLUMNS
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        comment_text: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.INTEGER, 
-            references: {
-                model: 'post',
-                key: 'id'
-            }
-        }
+  {
+    // THESE ARE THE COLUMNS
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
-    }
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
 );
 
 module.exports = Comment;
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const routes = require('./controllers');
-const session = require('express-session');
-const sequelize = require('./config/connection');
+const express = require("express");
+const routes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -26,15 +25,13 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // turn on routes
 app.use(routes);
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
